de9761ee4a57d5f2472fbd30def41492e6321a83
ulugbek101 <thedevu101@gmail.com>
2023-09-15 20:46:50 +0500

    Fix bug in urlpatterns list

--- PROJECT/urls.py
+++ PROJECT/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('chat/', include('chat.urls'))
+    path('chat/', include('chat.urls')),
     path('auth', include('auth.urls'))
 ]
